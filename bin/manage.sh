09de4f1cfe42baf75efb15cdf0b917c137373657
Tim Gross <tim@0x74696d.com>
2016-03-15 11:38:09 -0400

    Fix clustering of master+data nodes
    - Assign master+data nodes to elasticsearch-master service name
    - Change onStart behavior to allow master+data nodes to retry a few times
      if there is no other master yet.

--- bin/manage.sh
+++ bin/manage.sh
@@ -1,30 +1,58 @@
 #!/bin/bash
 
+MASTER=null
+
 if [[ -z ${CONSUL} ]]; then
     echo "Missing CONSUL environment variable"
     exit 1
 fi
 
 onStart() {
-    MASTER=null
-    while true
-    do
-        # get the list of ES master-only nodes from Consul
-        MASTER=$(curl -Ls --fail http://${CONSUL}:8500/v1/catalog/service/elasticsearch-master | jq -r '.[0].ServiceAddress')
-        if [[ $MASTER != "null" ]] && [[ -n $MASTER ]]; then
-            break
-        fi
-        # if this is the first master-only node, use itself to bootstrap
-        if [ ${ES_NODE_MASTER} == true ] && [ ${ES_NODE_DATA} == false ]; then
-            MASTER=127.0.0.1
-            break
-        fi
-        # this is not a master-only node and there are not master-only
-        # nodes up yet, so wait and retry
-        sleep 1.7
-    done
+    # happy path is that there's a master available and we can cluster
+    configureMaster
 
-    # update discovery.zen.ping.unicast.hosts
+    # data-only nodes can only loop until there's a master available
+    if [ ${ES_NODE_MASTER} == false ]; then
+        while true
+        do
+            sleep 1.7
+            configureMaster
+        done
+        exit 0
+    fi
+
+    # for a master+data node, we'll retry to see if there's another
+    # master in the cluster in the process of starting up. But we
+    # bail out if we exceed the retries and just bootstrap the cluster
+    if [ ${ES_NODE_DATA} == true ]; then
+        local n=0
+        until [ $n -ge 2 ]
+        do
+            sleep 1.7
+            configureMaster
+        done
+    fi
+
+    # for a master-only node or master+data node that's exceeded the
+    # retry attempts, we'll assume this is the first master and bootstrap
+    # the cluster
+    MASTER=127.0.0.1
+    replace
+}
+
+# get the list of ES master nodes from Consul
+configureMaster() {
+    MASTER=$(curl -Ls --fail http://${CONSUL}:8500/v1/catalog/service/elasticsearch-master | jq -r '.[0].ServiceAddress')
+    if [[ $MASTER != "null" ]] && [[ -n $MASTER ]]; then
+        replace
+        exit 0
+    fi
+    # if there's no master we fall thru and let the caller figure
+    # out what to do next
+}
+
+# update discovery.zen.ping.unicast.hosts
+replace() {
     REPLACEMENT=$(printf 's/^discovery\.zen\.ping\.unicast\.hosts.*$/discovery.zen.ping.unicast.hosts: ["%s"]/' ${MASTER})
     sed -i "${REPLACEMENT}" /etc/elasticsearch/elasticsearch.yml
 }
